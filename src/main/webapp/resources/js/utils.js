23f5640312b9128a3265cc7c90ffcaf833a4d2a7
Drorzz <arhorus@gmail.com>
2014-08-21 19:07:03 +0300

    Add tooltip to reports list

--- src/main/webapp/resources/js/utils.js
+++ src/main/webapp/resources/js/utils.js
@@ -4,29 +4,3 @@
 function redirectToPath(urlPath) {
     window.location.pathname = urlPath;
 }
-
-function getTooltipId(id){
-    return id+'Tooltip';
-}
-
-function showReportTooltip(id){
-    var mainReport = document.getElementById(id);
-    var tooltip = document.getElementById(getTooltipId(id));
-    if(mainReport.innerHTML.trim().length > 0) {
-        tooltip.innerHTML = mainReport.innerHTML;
-        tooltip.style.display = "block";
-    }
-}
-
-
-//TODO ToolTip сделать плавным, с отсрочкой и что бы не моргал или залипал если всплывает в с отступом
-function hideReportTooltip(id) {
-//    if(even.relatedTarget.id == id && even.target.id==getTooltipId(id)){
-////        alert("0");
-//        return;
-//    }
-////    alert("1");
-    var tooltip = document.getElementById(getTooltipId(id));
-    tooltip.innerHTML = "";
-    tooltip.style.display = "none";
-}
